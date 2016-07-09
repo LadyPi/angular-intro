ad4c80c99f05930c01db382e907ab90099003088
LadyPi <ms.vanessa.farias.dm@gmail.com>
2016-07-09 15:54:18 -0700

    initial commit: creates .gitignore, app.js, bower_components(added to .gitignore), and index.html

---
+++ ngFun/app.js
@@ -0,0 +1,35 @@
+var app = angular.module("ngFun", []);
+
+app.controller("PokemonCtrl", function($scope) {
+	// you want to add scope to your object 
+	// so that'pokemon' is shared by app.js and view
+  
+  $scope.catchphrase = "gotta catch 'em all!";	
+
+  $scope.pokemon = [
+  	{
+  	  Ndex: 25,
+  	  name: 'Pikachu',
+  	  type: 'Electric'
+  	},
+  	{  Ndex: 10,
+  	   name: 'Caterpie',
+  	   type: 'Bug' 
+  	},
+  	{
+  	  Ndex: 39,
+  	  name: 'Jigglypuff',
+  	  type: 'Fairy'
+  	},
+  	{
+  	  Ndex: 94,
+  	  name: 'Gengar',
+  	  type: 'Ghost' 	
+  	},
+  	{
+  	  Ndex: 143,
+  	  name: 'Snorlax',
+  	  type: 'Normal'
+  	}
+  ];
+});
